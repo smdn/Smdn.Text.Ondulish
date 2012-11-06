b93ec35767134e158d815d34bb1669cc0035bc61
smdn <smdn@smdn.jp>
2012-11-06 22:49:17 +0900

    updated script

--- external/mecab/make.sh
+++ external/mecab/make.sh
@@ -2,23 +2,31 @@
 
 basedir=$(cd $(dirname $0) && pwd)
 wrapperdir=$basedir/../../core/MeCab/
+installdir=$basedir/mecab
+
+package_mecab=mecab-0.994
+package_mecab_ipadic=mecab-ipadic-2.7.0-20070801
 
 # make mecab
 cd $basedir
 
-if [ -d ./mecab-* ] ; then
-  tar -xvf ./mecab-*.tar.*
+if [ ! -d $package_mecab ]; then
+  tar -xvf ${package_mecab}.tar.*
+  cd $package_mecab
+  ./configure --enable-shared --enable-static --with-charset=utf8 --prefix=$installdir
+else
+  cd $package_mecab
 fi
-cd mecab-*
 
-./configure --enable-shared --enable-static --with-charset=utf8 --prefix=/usr/local/mecab
 make
+make install
 
 # make mecab wrapper
 cd swig
 
 swig -csharp -o MeCab_wrap.cxx -namespace MeCab -dllimport libmecab -c++ MeCab.i
-g++ -shared -s MeCab_wrap.o ../src/libmecab.a -o libmecab.so
+g++ -fPIC -O2 -c MeCab_wrap.cxx -I../src/
+g++ -shared -s MeCab_wrap.o ../src/.libs/libmecab.a -o libmecab.so
 mcs /t:library /out:MeCab.dll *.cs
 
 # copy mecab wrapper
@@ -28,15 +36,23 @@ cp -t $wrapperdir MeCab.dll libmecab.so
 
 
 # make mecab dictionary
+export DYLD_LIBRARY_PATH=$installdir/lib:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$installdir/lib:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=$installdir/include
+PATH=$installdir/bin:$PATH
+
 cd $basedir
 
-if [ -d ./mecab-ipadic-* ] ; then
-  tar -xvf ./mecab-ipadic-*.tar.*
+if [ ! -d $package_mecab_ipadic ]; then
+  tar -xvf ${package_mecab_ipadic}.tar.*
+  cd $package_mecab_ipadic
+  ./configure --with-charset=utf8 --prefix=$installdir
+else
+  cd $package_mecab_ipadic
 fi
-cd mecab-ipadic-*
 
-./configure --with-charset=utf8 --prefix=/usr/local/mecab
 make
+make install
 
 # copy mecab dictionary
 mkdir -p $wrapperdir/dic/ipadic
