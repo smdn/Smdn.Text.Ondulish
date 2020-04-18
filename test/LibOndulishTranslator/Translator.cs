7398286069b53b46e2473b0772652c7d7865d4ab
smdn <smdn@smdn.jp>
2020-04-18 17:07:56 +0900

    add test project

---
+++ test/LibOndulishTranslator/Translator.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Smdn.Applications.OndulishTranslator {
+  [TestFixture]
+  public class TranslatorTests {
+    [Test]
+    public void Test()
+    {
+      Assert.Fail("TODO");
+    }
+  }
+}
