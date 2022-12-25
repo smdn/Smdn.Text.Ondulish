804dc56566d15ae4ea29cfe0e5b89a825d61864e
smdn <smdn@smdn.jp>
2022-12-25 12:50:26 +0900

    add examples

---
+++ examples/hello-ondulish-world/Program.cs
@@ -0,0 +1,6 @@
+﻿using Smdn.Text.Ondulish;
+
+var translator = new Translator();
+
+Console.WriteLine(translator.Translate("こんにちは、オンドゥル語の世界！"));
+Console.WriteLine(translator.Translate("本当に裏切ったんですか!?"));
