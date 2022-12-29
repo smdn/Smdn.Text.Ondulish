947bb9a0f747252005ff357fcab3064f39ab4cbf
smdn <smdn@smdn.jp>
2022-12-29 21:28:08 +0900

    install dependency packages for build on GitHub actions

---
+++ eng/dependencies/install-deps.mk
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
+# SPDX-License-Identifier: MIT
+#
+# This script installs the dependent apt packages to compile and run
+# MeCab shared library, especially on GitHub Actions' runner image.
+#
+install-buildtime-deps-ubuntu.22.04.stamp:
+	sudo apt-get install -y \
+	  autotools-dev \
+	  automake1.11
+	touch $@
+
+install-buildtime-deps-ubuntu.22.04: install-buildtime-deps-ubuntu.22.04.stamp
+
+.PHONY: install-buildtime-deps-ubuntu.22.04
